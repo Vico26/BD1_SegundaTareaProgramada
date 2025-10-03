d8d53ec7eb4878b5b21d84fb2209b78b36551c14
Victoria <130781096+Vico26@users.noreply.github.com>
2025-10-02 23:37:35 -0600

    updated
    Co-Authored-By: OrcaAusdauer <115200790+OrcaAusdauer@users.noreply.github.com>

--- obtenerEmpleados.js
+++ obtenerEmpleados.js
@@ -1,2 +1,4 @@
 const{sql,config}=require('./db'); //importacion del modulo de la base de datos
-async function obtenerEmpleados(){} //funcion asincrona para obtener los empleados
+async function obtenerEmpleados(){
+    
+} //funcion asincrona para obtener los empleados
