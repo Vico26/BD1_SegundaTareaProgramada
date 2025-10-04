8eb9b42a7c746deb1e0141b28fd40967993f2e97
Victoria <130781096+Vico26@users.noreply.github.com>
2025-10-03 23:43:38 -0600

    ObtenerEmpleado
    Funtion added

--- obtenerEmpleados.js
+++ obtenerEmpleados.js
@@ -1,4 +1,12 @@
 const{sql,config}=require('./db'); //importacion del modulo de la base de datos
 async function obtenerEmpleados(){
-    
+    try{
+        let pool=await sql.connect(config); //conexion a la base de datos
+        let result=await pool.request().execute('sp_ObtenerEmpleados'); //ejecucion del procedimiento almacenado
+        console.log("Resultados: ",result.recordset); //imprimir el resultado en la consola\
+        sql.close(); //cerrar la conexion
+        return result.recordset; //retornar el resultado
+    }catch(error){
+        console.log(error); //imprimir el error en la consola
+    }
 } //funcion asincrona para obtener los empleados
